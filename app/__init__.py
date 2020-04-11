204609680d0ede54ced140557466ec06b54dfd52
Sandra Lopez Zamora <slopezza@outlook.com>
2020-04-11 18:06:30 -0400

    css, html added

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,15 @@ Created on Fri April  3 2020
 """
 
 from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap  # To use bootstrap styles
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+app.static_folder = 'static'
+
+bootstrap = Bootstrap(app)
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template, flash, redirect, url_for, request
 from app import app
+
+from flask_bootstrap import Bootstrap
+
+# Redirect to "next" page
+from werkzeug.urls import url_parse
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
