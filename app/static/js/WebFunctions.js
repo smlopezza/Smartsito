5a8be447233b75bed7e8488057b6379e1a7e2391
Sandra Lopez Zamora <slopezza@outlook.com>
2020-04-20 09:36:59 -0400

    Total row added to savings

--- app/static/js/WebFunctions.js
+++ app/static/js/WebFunctions.js
@@ -13,6 +13,24 @@ $(document).ready(function() {
     {"className": "dt-center", "targets": "_all"}
   ]
 
+  });
+
+
+  var tableSavings = $('#SavingsConsumer').DataTable({
+    "retrieve": true,
+    "stateSave": true,  // to save pagination on refresh
+    "scrollY": "50vh",
+    "scrollCollapse": true,
+    "ordering": true,
+    dom: "frt",
+    //"autoWidth": true,
+    'order': [[0, 'asc']],
+    "columnDefs": [
+    {"className": "dt-center", "targets": "_all"}
+  ]
+
+  
+
 
 
   });
