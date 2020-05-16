b60c52390d1b1810bee455f26ccbbb21be4b3190
Sandra Lopez Zamora <slopezza@outlook.com>
2020-05-15 21:10:14 -0400

    Changes in dashboard

--- app/static/js/WebFunctions.js
+++ app/static/js/WebFunctions.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
+// https://www.codeply.com/go/7XYosZ7VH5
+  // Hide submenus
+$('#body-row .collapse').collapse('hide');
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left');
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
 
   var tableStore = $('#RetailerStore').DataTable({
     "retrieve": true,
@@ -29,7 +58,7 @@ $(document).ready(function() {
     {"className": "dt-center", "targets": "_all"}
   ]
 
-  
+
 
 
 
